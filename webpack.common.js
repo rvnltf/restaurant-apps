1120fa3c352a353cee0e76b104463c5bebd6d6d7
rvnltf <aldirivan56@gmail.com>
2021-06-24 22:55:38 +0700

    creating service worker but still error

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -22,10 +23,10 @@ module.exports = {
         ],
       },
       {
-          test: /\.(png|svg|jpg|jpeg|gif)$/,
-          use: [
-              'file-loader',
-          ],
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader',
+        ],
       },
     ],
   },
@@ -41,6 +42,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/'),
         },
       ],
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
     }),
   ],
 };
