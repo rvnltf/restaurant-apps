a05e902ba3d9f18caf403a03a78013d726d52c9a
rvnltf <aldirivan56@gmail.com>
2021-07-09 20:24:12 +0700

    Creating Unlike Restaurant Spec

---
+++ specs/unlikeRestaurantSpec.js
@@ -0,0 +1,41 @@
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb';
+
+const addLikeButtonContainer = () => {
+  document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+};
+
+describe('Unliking A Restaurant', () => {
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+  });
+
+  it('should display unlike widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this restaurant"]'))
+      .toBeTruthy();
+  });
+
+  it('should not display like widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this restaurant"]'))
+      .toBeFalsy();
+  });
+});
