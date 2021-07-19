35b3ac7ed54fde6b0b30242ce3c223ec1849afb9
rvnltf <aldirivan56@gmail.com>
2021-07-19 23:08:36 +0700

    Cache Key to Static

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
   BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_IMAGE_URL_LARGE: 'https://restaurant-api.dicoding.dev/images/large/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'PetoResto-1',
   DATABASE_NAME: 'restaurant-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
